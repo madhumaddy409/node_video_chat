364d109b08ef1fc8f044804a6329803c8fee02cb
madhumaddy409 <itech.maddy.tm@gmail.com>
2021-01-19 13:55:38 +0530

    profile

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   // host: '/',
   // port: '3001'
-  host: '/',
+  host: '/localhost',
   port: 9000,
   path: '/testvideochatdemo'
 })
